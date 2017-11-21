2aebdcb5ec3b165b3f4d6c68d031c529617c3c93
Alec Holmes <alecholmez@me.com>
2017-11-21 12:20:28 -0500

    added http service. break code out. changed logger

--- main.go
+++ main.go
@@ -1,108 +1,105 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 
 	"github.com/BurntSushi/toml"
-	"github.com/google/uuid"
-	"github.com/rs/zerolog/log"
+	"github.com/deciphernow/gm-fabric-go/middleware"
+	"github.com/gorilla/mux"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/hlog"
 )
 
-type service struct {
-	Name     string   `toml:"name"`
-	Routes   []string `toml:"routes"`
-	Location string   `toml:"location"`
-	Load     string   `toml:"load"`
-}
-
-type services struct {
-	Service []service
-}
+const (
+	wgKey key = iota
+)
 
 var (
-	settings = flag.String("services", "./services.toml", "Path to services.toml")
+	useHTTP = flag.Bool("http", true, "Use HTTP server")
+	config  = flag.String("config", "./services.toml", "Path to services.toml")
 )
 
 func main() {
 	flag.Parse()
+	log := zerolog.New(os.Stderr).With().Timestamp().Logger().
+		Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 	var s services
 	var wg sync.WaitGroup
 	rand.Seed(time.Now().UnixNano())
 
-	if _, err := toml.DecodeFile(*settings, &s); err != nil {
-		log.Print(err)
-		return
+	if *config != "" {
+		if _, err := toml.DecodeFile(*config, &s); err != nil {
+			log.Error().Err(err).Msg("Failed to decode config file")
+			return
+		}
+
+		log.Info().Msg("simulating load...")
+		for _, service := range s.Service {
+			wg.Add(1)
+			finished := make(chan bool, 1)
+			go hitService(service, finished, log)
+			go isFinished(finished, service, &wg, log)
+		}
 	}
 
-	log.Print("simulating load...")
-	for _, service := range s.Service {
+	if *useHTTP {
+		router := mux.NewRouter()
+		router.HandleFunc("/load", Load).Methods("POST")
+		router.HandleFunc("/loads", Load).Methods("POST")
+
+		stack := middleware.Chain(
+			middleware.MiddlewareFunc(hlog.NewHandler(log)),
+			middleware.MiddlewareFunc(hlog.AccessHandler(func(r *http.Request, status int, size int, duration time.Duration) {
+				hlog.FromRequest(r).Info().
+					Str("method", r.Method).
+					Str("path", r.URL.String()).
+					Int("status", status).
+					Int("size", size).
+					Dur("duration", duration).
+					Msg("Access")
+			})),
+			withServiceWG(&wg),
+		)
+
+		s := http.Server{
+			Addr:    os.Getenv("SERVICE_ADDR"),
+			Handler: stack.Wrap(router),
+		}
+
 		wg.Add(1)
-		finished := make(chan bool, 1)
-		go hitService(service, finished, &wg)
-		go isFinished(finished, service, &wg)
+		go func() {
+			log.Info().Msg("Service listening on: " + os.Getenv("SERVICE_ADDR"))
+			log.Fatal().Err(s.ListenAndServe()).Msg("Error when starting service")
+			wg.Done()
+		}()
 	}
 
 	wg.Wait()
-	log.Print("Exiting. . .")
+	log.Info().Msg("Exiting. . .")
 }
 
-func hitService(s service, finished chan bool, wg *sync.WaitGroup) {
-	var rwg sync.WaitGroup
-	client := &http.Client{}
+func withServiceWG(wg *sync.WaitGroup) middleware.Middleware {
+	return middleware.MiddlewareFunc(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			r = r.WithContext(context.WithValue(r.Context(), wgKey, wg))
+			next.ServeHTTP(w, r)
+		})
+	})
+}
 
-	for _, route := range s.Routes {
-		rwg.Add(1)
-		go func(route, location, load string) {
-			req, err := http.NewRequest("GET", location+route, nil)
-			if err != nil {
-				log.Print(err)
-			}
-
-			req.Close = true
-			req.Header.Add("x-request-id", uuid.New().String())
-
-			for {
-				resp, err := client.Do(req)
-				if err != nil {
-					log.Print(err)
-					break
-				}
-				resp.Body.Close()
-
-				switch {
-				case load == "light":
-					time.Sleep(time.Millisecond * time.Duration(randomInt(500, 1500)))
-				case load == "heavy":
-					time.Sleep(time.Millisecond * time.Duration(randomInt(5, 350)))
-				default:
-					time.Sleep(time.Second * 2)
-				}
-			}
-			rwg.Done()
-		}(route, s.Location, s.Load)
+func getServiceWG(ctx context.Context) *sync.WaitGroup {
+	wg, ok := ctx.Value(wgKey).(*sync.WaitGroup)
+	if !ok {
+		log.Fatal("Failed to retrieve waitgroup from context")
 	}
 
-	rwg.Wait()
-	finished <- true
-}
-
-func randomInt(min, max int) int {
-	return rand.Intn(max-min) + min
-}
-
-func isFinished(finished chan bool, service service, wg *sync.WaitGroup) {
-	for {
-		select {
-		case fin := <-finished:
-			if fin {
-				wg.Done()
-				log.Print("Finished pinging " + service.Name)
-			}
-		}
-	}
+	return wg
 }
